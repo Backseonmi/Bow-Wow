30a4e1b0e52029fafb0ec68149d20a2513813aaa
biyakim <w2116@e-mirim.hs.kr>
2023-05-26 23:27:27 +0900

    CORS 이슈, Proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios';
 
-export default function LandingPage() {
+function LandingPage() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => {console.log(response)})
+    },[])
   return (
     <div>
       LanginPage 렌딩페이지입니다.
     </div>
   )
 }
+
+export default LandingPage;
