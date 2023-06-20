6223d5093e4e5706bd55df1d26fe70d4496e566a
Backseonmi <w2102@e-mirim.hs.kr>
2023-06-20 20:40:05 +0900

    Detail

--- src/community/CommunityDetail.js
+++ src/community/CommunityDetail.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { doc, getDoc, deleteDoc, db, useCurrentUser } from '../firebase';
-import { ref, getDownloadURL } from 'firebase/storage';
+import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 import styles from './CommunityDetail.module.css';
 import { useHistory } from 'react-router-dom';
-import { storage } from '../firebase'; // storage 추가
+import { storage } from '../firebase';
 
 const CommunityDetail = (props) => {
   const [post, setPost] = useState(null);
@@ -29,6 +29,50 @@ const CommunityDetail = (props) => {
       fetchPost();
     }
   }, [postId]);
+
+  // 이미지 업로드 및 해상도 조절
+const handleImageUpload = async (file) => {
+  try {
+    const storageRef = ref(storage, 'images/' + file.name);
+    await uploadBytes(storageRef, file);
+
+    const imageUrl = await getDownloadURL(storageRef);
+
+    // 해상도 조절 또는 다른 작업 수행
+    const image = new Image();
+    image.src = imageUrl;
+
+    const canvas = document.createElement('canvas');
+    const maxWidth = 800; // 원하는 최대 가로 크기
+    const maxHeight = 600; // 원하는 최대 세로 크기
+
+    let width = image.width;
+    let height = image.height;
+
+    // 가로 또는 세로 크기가 최대 크기를 초과하면 크기 조절
+    if (width > maxWidth || height > maxHeight) {
+      if (width > height) {
+        height *= maxWidth / width;
+        width = maxWidth;
+      } else {
+        width *= maxHeight / height;
+        height = maxHeight;
+      }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, width, height);
+
+    const resizedImageUrl = canvas.toDataURL('image/jpeg', 0.8); // 압축률 조절 가능 (0.8은 80% 압축)
+
+    return resizedImageUrl;
+  } catch (error) {
+    console.log('이미지 업로드 중 오류가 발생했습니다.', error);
+  }
+};
 
   // 이미지 다운로드 URL 가져오기
   useEffect(() => {
@@ -57,7 +101,7 @@ const CommunityDetail = (props) => {
     try {
       await deleteDoc(doc(db, 'posts', post.id));
       alert('게시물이 성공적으로 삭제되었습니다.');
-      history.push('/community'); // bow로 리디렉션
+      history.push('/community');
     } catch (error) {
       console.log('게시물 삭제 중 오류가 발생했습니다.', error);
     }
